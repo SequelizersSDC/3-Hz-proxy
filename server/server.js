b55598d8208eeb2ccac5fcc49f8edabec7cbb039
3-Hz <edward.h.zhao@gmail.com>
2021-01-29 19:31:48 -0600

    added compression middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 
 const server = express();
 const PORT = process.env.PORT || 1337;
+
+server.use(compression());
 
 server.use(express.static(path.resolve(__dirname, '../public')));
 
